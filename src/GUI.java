3129c92208157b15459ad33da409267ecaa1a38d
BogdanTodor <Bogdan3101@hotmail.com>
2019-05-18 20:29:46 +1000

    Created mouse click event and plot button which records the position of the mouse relative to the drawing canvas when enabled

--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,20 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.filechooser.FileFilter;
+import javax.swing.plaf.metal.MetalToggleButtonUI;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.Line2D;
+import java.awt.event.*;
 import java.io.File;
 
 public class GUI extends JFrame implements Runnable {
 
     public static final int WIDTH = 500;
     public static final int HEIGHT = 400;
+    public static double dynamicWidth;
+    public static double dynamicHeight;
+    public static boolean plotToggle;
 
     private void createAndShowGUI() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,6 +25,7 @@ public class GUI extends JFrame implements Runnable {
 
         repaint();
         this.setVisible(true);
+
     }
 
     public static void addComponentsToPane(Container pane) {
@@ -28,6 +33,7 @@ public class GUI extends JFrame implements Runnable {
         GridBagConstraints c = new GridBagConstraints();
         JPanel mainPanel, leftPanel, topPanel, rightPanel, bottomPanel;
         JButton newButton, openButton, saveButton;
+        JToggleButton plotButton;
 
         newButton = new JButton("New");
         newButton.addActionListener(new ActionListener() {
@@ -63,6 +69,45 @@ public class GUI extends JFrame implements Runnable {
         c.gridy = 0;
         pane.add(saveButton, c);
 
+        // Moved panel creation here to not break GUI design
+        mainPanel = new drawingPanel();
+
+        // Create a mouse listener object
+        MouseListener mousePosition = new MouseClickEvent();
+
+        // Adds a toggle button which allows user to trigger mouse clicks as coordinates for plotting.
+        plotButton = new JToggleButton("Plot");
+        plotButton.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED){
+                    System.out.println("Plotting enabled");
+                    plotButton.setUI(new MetalToggleButtonUI(){
+                        @Override
+                        protected Color getSelectColor(){
+                            return Color.GREEN;
+                        };
+                    });
+                    plotToggle = true;
+                    mainPanel.addMouseListener(mousePosition);
+
+                } else if(e.getStateChange() == ItemEvent.DESELECTED){
+                    System.out.println("Plotting disabled");
+                    plotToggle = false;
+                    mainPanel.removeMouseListener(mousePosition);
+//                    for(int i = 1; i<mainPanel.getMouseListeners().length-1;i++){
+//                        mainPanel.removeMouseListener(mainPanel.getMouseListeners()[i]);
+//                    }
+                }
+
+            }
+        });
+
+        c.anchor = GridBagConstraints.LINE_START;
+        c.gridx = 3;
+        c.gridy = 0;
+        pane.add(plotButton, c);
+
         topPanel = new JPanel();
         topPanel.setBackground(Color.LIGHT_GRAY);
         topPanel.setBorder(new LineBorder(Color.GRAY));
@@ -72,7 +117,7 @@ public class GUI extends JFrame implements Runnable {
         leftPanel.setBackground(Color.LIGHT_GRAY);
         setConstraints(leftPanel,0,1,2,1,0,0,c,pane);
 
-        mainPanel = new drawingPanel();
+//        mainPanel = new drawingPanel();
         setConstraints(mainPanel,2,1,1,1,1,1,c,pane);
 
 
@@ -93,7 +138,45 @@ public class GUI extends JFrame implements Runnable {
         };
         bottomPanel.setBackground(Color.LIGHT_GRAY);
         setConstraints(bottomPanel,0,2,4,1,0,0,c,pane);
+
+        mainPanel.addComponentListener(new canvasResizeEvent());
     }
+
+    public static class MouseClickEvent implements MouseListener{
+        public void mouseClicked(MouseEvent e) {
+            if(plotToggle){
+            System.out.println(e.getX()/dynamicWidth + ", " + e.getY()/dynamicHeight);
+//            System.out.println(e.getX());
+//            System.out.println(dynamicHeight);
+            }
+        }
+        public void mousePressed(MouseEvent e){}
+        public void mouseReleased(MouseEvent e){}
+        public void mouseEntered(MouseEvent e){}
+        public void mouseExited(MouseEvent e){}
+    }
+
+    public static class canvasResizeEvent implements ComponentListener{
+        public void componentResized(ComponentEvent c) {
+//            System.out.println(c.getComponent().getHeight());
+//            System.out.println(WIDTH);
+//            System.out.println((c));
+            dynamicHeight = c.getComponent().getHeight();
+            dynamicWidth = c.getComponent().getHeight();
+        }
+        public void componentMoved(ComponentEvent c) {
+//            System.out.println(c);
+        }
+        public void componentShown(ComponentEvent c) {
+            System.out.println(c);
+        }
+        public void componentHidden(ComponentEvent c) {
+//            System.out.println(c);
+        }
+
+    }
+
+
 
     public static void setConstraints(JPanel panel, int gridx, int gridy, int gridwidth, int gridheight, int weightx, int weighty, GridBagConstraints c, Container pane) {
         c.fill = GridBagConstraints.BOTH;
@@ -183,4 +266,5 @@ class drawingPanel extends JPanel {
             shape.drawShape(g, size);
         }
     }
+
 }
